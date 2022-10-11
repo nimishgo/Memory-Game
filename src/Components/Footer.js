be9776e2e41f76e1b5097ed736c3884d3f649e2b
nimishgo <nimish.2711@gmail.com>
2022-10-12 00:31:18 +0530

    initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/footer.css'
+
+const Header = () => {
+  return (
+    <div className='footer'>
+        <h4>Made by nimishgo</h4>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/main.css'
+
+const Main = () => {
+  return (
+    <div className='container'>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+export default Main
