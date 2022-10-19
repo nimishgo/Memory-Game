781a6d762998410260ec6619e397afdc1727bc46
nimishgo <nimish.2711@gmail.com>
2022-10-19 13:10:07 +0530

    feat: added section for scores

---
+++ src/Components/Scores.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/main.css'
+
+const Scores = () => {
+  return (
+    <div className='scoreBoard'>  
+    <div>Max. Score : 10</div>
+    <div>Your Score : </div>
+    <div>Highest Score : </div>
+    </div>
+  )
+}
+
+export default Scores;
