041b2aee0923cf01c8c669368970ef9d7ad484a2
nimishgo <nimish.2711@gmail.com>
2022-10-20 15:40:35 +0530

    fix: score board

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Cards from './Cards'
+import {list} from './Data'
 import './styles/main.css'
 
 const Main = () => {
+
   return (
     <div className='container'>
-      <p>Hello World</p>
+      <Cards arr={list}/>
     </div>
   )
 }
