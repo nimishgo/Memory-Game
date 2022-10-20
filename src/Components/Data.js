61d76bdf7277e49626b735192ceb9d17fe906c80
nimishgo <nimish.2711@gmail.com>
2022-10-20 15:37:57 +0530

    added data list

---
+++ src/Components/Data.js
@@ -0,0 +1,65 @@
+export const list = [
+  {
+    id : 1,
+    name : 'anakin skywalker',
+    src : '../assets/anakinSkywalker.png',
+    isClicked : false,
+  },
+  {
+    id : 2,
+    name : 'chewbacca',
+    src : '../assets/chewbacca.png',
+    isClicked : false,
+  },
+  {
+    id : 3,
+    name : 'darth maul',
+    src : '../assets/darthMaul.png',
+    isClicked : false,
+  },
+  {
+    id : 4,
+    name : 'darth vader',
+    src : '../assets/dartVader.png',
+    isClicked : false,
+  },
+  {
+    id : 5,
+    name : 'jar jar binks',
+    src : '../assets/jarjarBinks.png',
+    isClicked : false,
+  },
+  {
+    id : 6,
+    name : 'kylo ren',
+    src : '../assets/kyloRen.png',
+    isClicked : false,
+  },
+  {
+    id : 7,
+    name : 'luke skywalker',
+    src : '../assets/lukeSkywalker.png',
+    isClicked : false,
+  },
+  {
+    id : 8,
+    name : 'obi wan',
+    src : '../assets/obiWan.png',
+    isClicked : false,
+  },
+  {
+    id : 9,
+    name : 'r2d2',
+    src : "../assets/r2d2.png",
+    isClicked : false,
+  },
+  {
+    id : 10,
+    // /home/roci-nymysh/odin-projects/memory-card/src/assets/chewbacca.png
+    name : 'yoda',
+    src : '../assets/yoda.png',
+    isClicked : false,
+  }
+]
+
+// export list;
